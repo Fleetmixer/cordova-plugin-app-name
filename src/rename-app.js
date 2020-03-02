fd8c29b30af115de4cb0a8318a391692e0a69769
Hilderlan Herbert <hilderlan.herbert@gmail.com>
2020-03-02 17:49:10 -0300

    Fix for cordova 9

--- src/rename-app.js
+++ src/rename-app.js
@@ -58,9 +58,9 @@ module.exports = function (context) {
 function getConfigParser(context, config) {
 
     if (semver.lt(context.opts.cordova.version, '5.4.0')) {
-        ConfigParser = context.requireCordovaModule('cordova-lib/src/ConfigParser/ConfigParser');
+        ConfigParser = require('cordova-lib/src/ConfigParser/ConfigParser');
     } else {
-        ConfigParser = context.requireCordovaModule('cordova-common/src/ConfigParser/ConfigParser');
+        ConfigParser = require('cordova-common/src/ConfigParser/ConfigParser');
     }
 
     return new ConfigParser(config);
